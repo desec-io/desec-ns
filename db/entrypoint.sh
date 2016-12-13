f5be3442b9a554335eb13ff142b1dfd939e79d58
Peter Thomassen <peter@desec.io>
2016-12-13 13:57:01 +0100

    fix(ssl): adjust SSL file permissions on startup

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -3,4 +3,6 @@
 echo Preparing database configuration ...
 envsubst < /etc/mysql/conf.d/51-server.cnf.var > /etc/mysql/conf.d/51-server.cnf
 
+chgrp mysql /etc/ssl/private/*.pem
+
 /docker-entrypoint.sh "$@"
