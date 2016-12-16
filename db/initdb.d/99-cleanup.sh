357f7fe8b7cd723fe6ed06325fe7d405537ff218
Peter Thomassen <peter@desec.io>
2016-12-16 13:05:13 +0100

    feat(db): cleanup dumps after import, fixes #2

---
+++ db/initdb.d/99-cleanup.sh
@@ -0,0 +1,5 @@
+# https://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Remove dumps
+rm -v $DIR/2*.sql
