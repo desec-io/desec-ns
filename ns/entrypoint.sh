0aea6e6aae1ec81b102367b67bceb4e9a7ea9769
Peter Thomassen <peter@desec.io>
2020-01-27 19:29:42 +0100

    feat(): add multicast group for nsmaster's NOTIFY

--- ns/entrypoint.sh
+++ ns/entrypoint.sh
@@ -4,6 +4,12 @@
 # Ths is assuming that the stack-side network prefix is 172.16
 /sbin/ip route add 172.16.7.0/24 via 10.16.3.2
 
+# Route to contact VPN client
+/sbin/ip route add 10.8.0.0/24 via 10.16.3.2
+
+echo mgroup from eth2 group 239.1.2.3 > /etc/smcroute.conf
+/usr/sbin/smcroute -d
+
 # Manage credentials
 envsubst < /etc/powerdns/pdns.conf.var > /etc/powerdns/pdns.conf
 
