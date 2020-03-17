7bc859bd6b9a6e721117adc7b7f06711cbc62282
Peter Thomassen <peter@desec.io>
2020-03-17 22:05:55 +0100

    fix(): avoid building lmdb-backup by pulling explicitly

--- dump.sh
+++ dump.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker-compose -f docker-compose.yml -f docker-compose.dump.yml up replicator  # exits replicator when done
 docker-compose -f docker-compose.yml -f docker-compose.dump.yml down  # exits other containers as well
+docker-compose -f docker-compose.yml -f docker-compose.dump.yml pull lmdb-backup
 docker-compose -f docker-compose.yml -f docker-compose.dump.yml run lmdb-backup ./dump
--- load.sh
+++ load.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+docker-compose -f docker-compose.yml -f docker-compose.dump.yml pull lmdb-backup
 docker-compose -f docker-compose.yml -f docker-compose.dump.yml run lmdb-backup ./load "$1"
