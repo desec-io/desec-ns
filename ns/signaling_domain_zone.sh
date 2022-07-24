1ceb9bbe7024ddb7b6dadcfb9ef00e0c401e3957
Nils Wisiol <mail@nils-wisiol.de>
2022-07-24 15:29:12 -0400

    fix(ns): add NS record to Signaling Domain zone

--- ns/signaling_domain_zone.sh
+++ ns/signaling_domain_zone.sh
@@ -6,6 +6,7 @@ SIGNALING_DOMAIN="_signal.$DESEC_NS_NAME"
 pdnsutil create-zone "$SIGNALING_DOMAIN" || exit  # quit if zone exists
 pdnsutil replace-rrset "$SIGNALING_DOMAIN" "@" SOA \
     "${DESEC_NS_SIGNALING_DOMAIN_SOA_MNAME}. ${DESEC_NS_SIGNALING_DOMAIN_SOA_RNAME}. 0 86400 3600 2419200 3600"
+pdnsutil replace-rrset "$SIGNALING_DOMAIN" "@" NS 3600 "$DESEC_NS_NAME"
 pdnsutil import-zone-key "$SIGNALING_DOMAIN" <(base64 -d <<< "$DESEC_NS_SIGNALING_DOMAIN_ZONE_PRIVATE_KEY_B64")
 pdnsutil unset-presigned "$SIGNALING_DOMAIN"
 pdnsutil unset-nsec3 "$SIGNALING_DOMAIN"
