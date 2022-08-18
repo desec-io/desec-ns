e5482f49ce145fb26fd80d36c0ecd1f9bc03f093
Nils Wisiol <mail@nils-wisiol.de>
2022-08-18 09:59:44 +0200

    feat(lmdb-backup): clean up after dumping database

--- dump.sh
+++ dump.sh
@@ -3,3 +3,4 @@ docker-compose -f docker-compose.yml -f docker-compose.dump.yml up replicator  #
 docker-compose -f docker-compose.yml -f docker-compose.dump.yml down  # exits other containers as well
 docker-compose -f docker-compose.yml -f docker-compose.dump.yml pull lmdb-backup
 docker-compose -f docker-compose.yml -f docker-compose.dump.yml run lmdb-backup ./dump
+docker-compose -f docker-compose.yml -f docker-compose.dump.yml down
