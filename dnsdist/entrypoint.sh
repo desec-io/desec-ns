8ac78773283bc6d6566a7bb0309f3394757b36ae
Nils Wisiol <mail@nils-wisiol.de>
2022-08-31 19:40:26 +0200

    fix(): removes broken ip6 DoQ config and adjusts internal networks

--- dnsdist/entrypoint.sh
+++ dnsdist/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-iptables -t nat -A PREROUTING -p udp --dport 853 -j DNAT --to-destination 10.16.0.3:853
+iptables -t nat -A PREROUTING -p udp --dport 853 -j DNAT --to-destination 10.16.5.3:853
 iptables -t nat -A POSTROUTING -j MASQUERADE
-ip6tables -t nat -A PREROUTING -p udp --dport 853 -j DNAT --to-destination "${DESEC_NS_IPV6_PREFIX}:3"
-ip6tables -t nat -A POSTROUTING -j MASQUERADE
 
 exec dnsdist --supervised
