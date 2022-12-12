3d7062ddb0e3a16815888b061b29fba62e09368d
Peter Thomassen <peter@desec.io>
2022-12-12 11:51:07 +0100

    feat(dnsdist): add debug output to investigate dnsdist CLI disconnect

--- dnsdist/entrypoint.sh
+++ dnsdist/entrypoint.sh
@@ -3,4 +3,4 @@
 iptables -t nat -A PREROUTING -p udp --dport 853 -j DNAT --to-destination 10.16.5.3:853
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
-exec dnsdist --supervised
+exec dnsdist --supervised -v
