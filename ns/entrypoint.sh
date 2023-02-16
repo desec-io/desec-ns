7482f5b92fe0dbef4a5438ca3256bf14928ed3a5
Nils Wisiol <mail@nils-wisiol.de>
2023-02-16 20:23:19 +0100

    feat(ns): use Gentoo-based pdns optimized for sandybridge. No version pinning! Currently 4.7.3

--- ns/entrypoint.sh
+++ ns/entrypoint.sh
@@ -2,13 +2,13 @@
 
 # Route required for asking questions into the stack-side network via VPN
 # Ths is assuming that the stack-side network prefix is 172.16
-/sbin/ip route add 172.16.7.0/24 via 10.16.3.2
+ip route add 172.16.7.0/24 via 10.16.3.2
 
 # Route to contact VPN client
-/sbin/ip route add 10.8.0.0/24 via 10.16.3.2
+ip route add 10.8.0.0/24 via 10.16.3.2
 
-echo mgroup from eth2 group 239.1.2.3 > /etc/smcroute.conf
-/usr/sbin/smcroute -d
+echo phyint eth2 static-group 239.1.2.3 > /etc/mrouted.conf
+mrouted
 
 # Render configuration
 export _NPROC=$(nproc)
